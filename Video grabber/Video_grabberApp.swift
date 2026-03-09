e199a782b6199bac55aff7ac5a4ded0552cd4fb5
Carlos Solé <charlysole@gmail.com>
2026-03-09 12:53:34 -0300

    Initial Commit

---
+++ Video grabber/Video_grabberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_grabberApp.swift
+//  Video grabber
+//
+//  Created by Carlos Solé on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Video_grabberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
